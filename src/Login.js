0e955c3f1a9d5005239ee649f4504a54b5f02e23
payal patra <67522406+payalpatra@users.noreply.github.com>
2021-01-17 00:23:16 +0530

    Updated login logo

--- src/Login.js
+++ src/Login.js
@@ -23,7 +23,7 @@ function Login() {
     <div className="login">
       <div className="login__container">
         <img
-          src="https://logos-world.net/wp-content/uploads/2020/05/WhatsApp-Logo-700x394.png"
+          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
           alt="login__logo"
         />
         <div className="login__text">
